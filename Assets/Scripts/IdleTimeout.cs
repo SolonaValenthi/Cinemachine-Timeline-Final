4c0ba13b6877197b1f22a122114c99a209c70975
Nikolai Nelson <nikolainelson@comcast.net>
2024-01-04 06:04:39 -0800

    implemented idle camera system

--- Assets/Scripts/IdleTimeout.cs
+++ Assets/Scripts/IdleTimeout.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class IdleTimeout : MonoBehaviour
 {
+    [SerializeField] private PlayerCameras _playerCams;
+
+    private float _idleTime = 0.0f; // time since last input
+    private bool _isIdle = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,30 @@ public class IdleTimeout : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+
+        if (_isIdle == false && _idleTime > 10.0f)
+        {
+            _isIdle = true;
+            _playerCams.Idle();
+        }
+    }
+
+    private void CheckInput()
+    {
+        if (Input.anyKey || Input.GetAxis("Mouse X") != 0f || Input.GetAxis("Mouse Y") != 0f)
+        {
+            if (_isIdle)
+            {
+                _playerCams.WakeUp();
+            }
+
+            _isIdle = false;
+            _idleTime = 0.0f;
+        }
+        else
+        {
+            _idleTime += Time.deltaTime;
+        }
     }
 }
--- Assets/Scripts/PlayerCameras.cs
+++ Assets/Scripts/PlayerCameras.cs
@@ -8,6 +8,7 @@ public class PlayerCameras : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera _vcamShip;
     [SerializeField] private CinemachineVirtualCamera _vcamCockpit;
     [SerializeField] private GameObject _cockpit;
+    [SerializeField] private GameObject[] _idleCams;
 
     MeshRenderer _shipRender;
 
@@ -40,17 +41,92 @@ public class PlayerCameras : MonoBehaviour
     {
         if (_cockpit.activeInHierarchy == true)
         {
+            _vcamShip.Priority = 100;
             _vcamCockpit.Priority = 10;
             _cockpit.SetActive(false);
-            _vcamShip.Priority = 100;
             _shipRender.enabled = true;
         }
         else
         {
+            _vcamCockpit.Priority = 100;
             _vcamShip.Priority = 10;
             _cockpit.SetActive(true);
-            _vcamCockpit.Priority = 100;
             _shipRender.enabled = false;
+        }
+    }
+
+    public void Idle()
+    {
+        _cockpit.SetActive(false);
+        _vcamCockpit.Priority = 10;
+        _vcamShip.Priority = 10;
+        _shipRender.enabled = true;
+
+        IdleSwitch(true);
+    }
+
+    public void WakeUp()
+    {
+        IdleSwitch(false);
+    }
+
+    private void IdleSwitch(bool idle)
+    {
+        if (idle)
+        {
+            StartCoroutine("IdleCycle");
+        }
+        else
+        {
+            _vcamShip.Priority = 100;
+            StopCoroutine("IdleCycle");
+        }
+    }
+
+    IEnumerator IdleCycle()
+    {
+        int camIndex = Random.Range(0, _idleCams.Length);
+
+        while (true)
+        {
+            for (int i = 0; i < _idleCams.Length; i++)
+            {
+                if (_idleCams[i].TryGetComponent(out CinemachineVirtualCamera vcam))
+                {
+                    if (i == camIndex)
+                    {
+                        vcam.Priority = 11;
+                    }
+                    else
+                    {
+                        vcam.Priority = 10;
+                    }
+                }
+                else if (_idleCams[i].TryGetComponent(out CinemachineBlendListCamera blend))
+                {
+                    if (i == camIndex)
+                    {
+                        blend.Priority = 11;
+                    }
+                    else
+                    {
+                        blend.Priority = 10;
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Failed to detect camera type of " + _idleCams[i]);
+                }
+            }
+
+            camIndex++;
+
+            if (camIndex > _idleCams.Length - 1)
+            {
+                camIndex = 0;
+            }
+
+            yield return new WaitForSeconds(10.0f);
         }
     }
 }
