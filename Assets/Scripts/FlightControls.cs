4623f1f78bac2a715b14b854ee4c49bf8ef46d6d
Nikolai Nelson <nikolainelson@comcast.net>
2024-01-05 05:32:36 -0800

    initial fligt control setup

--- Assets/Scripts/FlightControls.cs
+++ Assets/Scripts/FlightControls.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class FlightControls : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _rotSpeed;
+
+    private float _vertical;
+    private float _horizontal;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,31 @@ public class FlightControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ShipMovement();
+    }
+
+    private void ShipMovement()
+    {
+        _vertical = Input.GetAxis("Vertical");
+        _horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (_moveSpeed < 10.0f)
+            {
+                _moveSpeed += 0.2f;
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (_moveSpeed > 0.3f)
+            {
+                _moveSpeed -= 0.2f;
+            }
+        }
+
+        transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.left * _rotSpeed * _vertical * Time.deltaTime); // pitch control
+        transform.Rotate(Vector3.up * _rotSpeed * _horizontal * Time.deltaTime); // yaw control
     }
 }
