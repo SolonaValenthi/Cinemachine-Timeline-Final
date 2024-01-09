0d1b8b72ebb3b28ffe19ead0fd1d062d98d7614b
Nikolai Nelson <nikolainelson@comcast.net>
2024-01-09 05:08:42 -0800

    repair cutscene

--- Assets/Scripts/FlightControls.cs
+++ Assets/Scripts/FlightControls.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class FlightControls : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _rotSpeed;
+    [SerializeField] private PlayableDirector _repairDirector;
+    [SerializeField] private ParticleSystem[] _sideEngines;
+    [SerializeField] private ParticleSystem _mainEngine;
 
     private float _vertical;
     private float _horizontal;
@@ -35,11 +39,11 @@ public class FlightControls : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            _moveSpeed += 0.5f;
+            _moveSpeed += 0.3f;
         }
         if (Input.GetKey(KeyCode.Space))
         {
-            _moveSpeed -= 0.5f;
+            _moveSpeed -= 0.3f;
         }
         _moveSpeed = Mathf.Clamp(_moveSpeed, 0f, 15f);
 
@@ -47,6 +51,15 @@ public class FlightControls : MonoBehaviour
         transform.Rotate(Vector3.left * _rotSpeed * _vertical * Time.deltaTime); // pitch control
         transform.Rotate(Vector3.up * _rotSpeed * _horizontal * Time.deltaTime); // yaw control
         transform.Rotate(Vector3.forward * (_rotSpeed * 1.5f) * _roll * Time.deltaTime); // roll control
+
+        foreach (var emitter in _sideEngines)
+        {
+            var emission = emitter.emission;
+            emission.rateOverTime = _moveSpeed;
+        }
+
+        var mainEmission = _mainEngine.emission;
+        mainEmission.rateOverTime = _moveSpeed * 3;
     }
 
     public void CutsceneToggle()
@@ -58,6 +71,18 @@ public class FlightControls : MonoBehaviour
         else
         {
             _inCutscene = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Satellite")
+        {
+            if (_repairDirector.gameObject.activeInHierarchy == true)
+            {
+                _moveSpeed = 0.0f;
+                _repairDirector.Play();
+            }
         }
     }
 }
